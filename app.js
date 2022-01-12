65ae6efa95c9e4f6b94160c1284f7d7a0ad8dddf
sarah-svg <kjskillen@gmail.com>
2022-01-11 19:17:17 -0800

    updating style and added audio

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ import { getPokedex, getStarWarsPeople } from './api-fetch.js';
 const template = document.querySelector('#template');
 const list = document.querySelector('#list');
 const selectEl = document.querySelector('select');
+const audio = document.querySelector('audio');
+const main = document.querySelector('main');
 /// Validate the data from the API by console logging the data
 // ex: console.log(template, list);
 
@@ -20,6 +22,7 @@ const selectEl = document.querySelector('select');
 async function loadPokedex() {
     /// use the await keyword to wait for the data to be ready
     const pokedex = await getPokedex();
+    list.classList.add('pokemon');
     /// 1.  Validate the data from the API by console logging the data
         // ex: console.log(pokedex); check console to see the data
     /// 2 . Loop through the data and create a new element for each item in the array
@@ -31,13 +34,17 @@ async function loadPokedex() {
         const name = clone.querySelector('h2');
         const image = clone.querySelector('img');
         const type = clone.querySelector('h6');
+    
         /// Validate console.log(pokemon, name, image);
         /// 5. Set the textContent of the h1 element to the name, pokedex egg_group_1 of the pokemon
         /// But look at the data you can add more but dot notate to grab the data you want
-
-        name.textContent = pokemon.pokemon;
+        /// play audio using the audio element with the play method
+        audio.play();
+ 
+      
+        name.textContent = 'Name: ' + pokemon.pokemon;
   
-        type.textContent = pokemon.egg_group_1;
+        type.textContent = 'Egg: ' + pokemon.egg_group_1;
         image.src = pokemon.url_image;
         image.alt = pokemon.pokedex;
         /// 6. Append the clone to the list element
@@ -59,18 +66,19 @@ async function loadStarWars() {
     for (let person of starWars) {
         // console.log(person); to make sure you are looping through the data correctly
         /// 3. Create a variable to hold the clone of the template
+        list.classList.add('star-wars');
         const clone = template.content.cloneNode(true);
         /// 4. Grab the h1 element from the clone and set it to a variable
         const name = clone.querySelector('h2');
     
         const type = clone.querySelector('h6');
         /// Validate console.log(person, name, hair_color);
-        /// 5. Set the textContent of the h1 element to the name, starWars egg_group_1 of the person
+        /// 5. Set the textContent of the h1 element to the name, starWars of the person
         /// But look at the data you can add more but dot notate to grab the data you want
-
-        name.textContent = person.name;
+        audio.play();
+        name.textContent = ' Name: ' + person.name;
   
-        type.textContent = person.hair_color;
+        type.textContent = ' Hair color: ' + person.hair_color;
       
         /// 6. Append the clone to the list element
         list.appendChild(clone);
@@ -85,19 +93,26 @@ async function loadStarWars() {
 
 // set event listeners 
 selectEl.addEventListener('change', async(e) => { 
+
     if (e.target.value === 'pokemon') {
+        audio.src = './assets/pokemon.wav';
          // get user input
             // use user input to update state
             // update DOM to reflect the new state
         /// 1. Clear the list element to then render the new data
+        list.classList.remove('star-wars');
         list.innerHTML = '';
         await loadPokedex();
     } else if (e.target.value === 'star-wars') {
+        audio.src = './assets/star-wars.wav';
                // get user input
             // use user input to update state
             // update DOM to reflect the new state
         /// 1. Clear the list element to then render the new data
+        list.classList.remove('pokemon');
         list.innerHTML = '';
         await loadStarWars();
     }
+    /// Remove the class for each time the selectEl changes
+  
 });
